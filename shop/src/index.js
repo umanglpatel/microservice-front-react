c9e64521e0770556f33d1ded125039fd027a42db
Umang Patel <umang.patel@AMD.IBASET.COM>
2019-04-25 15:33:59 +0530

    Fix issue that was causing error if redux devtools is not installed in browser

--- shop/src/index.js
+++ shop/src/index.js
@@ -15,7 +15,8 @@ const rootReducer = combineReducers({
     auth: authReducer,
     user: userReducer
 });
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
